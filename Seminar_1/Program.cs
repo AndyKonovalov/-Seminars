a935e4b75d8810474a612475acc6fa1286c7c3af
Andrei <fmwar92@gmail.com>
2022-06-23 18:23:12 +0300

    output the square of the number to the console

---
+++ Seminar_1/Program.cs
@@ -0,0 +1,9 @@
+﻿// написать программу, принимающую на вход число и выдающую его квадрат
+Console.Write("Введите целое число: ");
+int number = Convert.ToInt32(Console.ReadLine()); // конвертируем строку в число, с помощью Convert.ToInt32 
+
+int squareNumber = number * number;
+
+Console.WriteLine ("Квадрат числа равен: " + squareNumber); // конкретинация склеивание
+
+//Console.WriteLine(squareNumber); 
