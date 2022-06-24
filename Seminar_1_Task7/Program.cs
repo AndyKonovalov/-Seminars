7563327ad1afd6b27490bd376ec32ab929ee9289
Andrei <fmwar92@gmail.com>
2022-06-24 14:49:37 +0300

    Learnd new operator (%)

---
+++ Seminar_1_Task7/Program.cs
@@ -0,0 +1,13 @@
+﻿int Number = 0;
+Console.Write("Введите трёхзначное число: ");
+int Number = Convert.ToInt32(Console.ReadLine());
+
+if (Number >= 100 && number <= 999)
+{
+    Number = Number % 10;
+    Console.WriteLine ("Последнее число " + Number);
+}
+else
+{
+    Console.WriteLine ("Error");
+}
