50612ae6da54c08c0f9e13f24c752271720ca18e
Andrei <fmwar92@gmail.com>
2022-06-27 21:34:54 +0300

    Delete number 4 from 546

---
+++ Seminar2Task1/Program.cs
@@ -0,0 +1,11 @@
+﻿//Напишите программу, которая выводит случайное трёхзначное 
+//число и удаляет вторую цифру этого числа.
+
+int number = new Random().Next(100, 1000);
+Console.WriteLine(number);
+int digitOne = number / 100;
+int digitThree = number % 10;
+//Console.WriteLine("результат " + digitOne + digitThree);
+
+int result = digitOne * 10 + digitThree * 1;
+Console.WriteLine(result);
