37f4ac1781212ed9e9e65bb6224e6d1a301d92b9
Andrei <fmwar92@gmail.com>
2022-07-01 13:30:39 +0300

    Bugfix

--- Seminar3_Task22/Program.cs
+++ Seminar3_Task22/Program.cs
@@ -4,7 +4,7 @@ int N = Convert.ToInt32(Console.ReadLine());
 int i = 1; // числа от 1 до N
 int d = 0; // квадрат числа N
 
-while (i <= N) // for(i = 1; i <= N; i++) - цикл for позволяет компактнее записать цикл while
+while (i <= N) // for(int i = 1; i <= N; i++) - цикл for позволяет компактнее записать цикл while
 {
     d = i * i;
     Console.WriteLine(d);
