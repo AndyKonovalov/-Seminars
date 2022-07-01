01d61dab01b563cccf524f02be8843a972aeedfd
Andrei <fmwar92@gmail.com>
2022-07-01 12:40:31 +0300

    Worked with Math.Sqrt, Math.Pow, Math.Round. Found the distance between points A and B by their coordinates

---
+++ Seminar3_Task21/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите первую координату точки А: ");
+int x1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите вторую координату точки А: ");
+int y1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите первую координату точки В: ");
+int x2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите вторую координату точки В: ");
+int y2 = Convert.ToInt32(Console.ReadLine());
+
+double dist = Math.Sqrt(Math.Pow(x1 - x2,2) + Math.Pow(y1 - y2,2)); // Math.Sqrt() -квадратный корень, Math.Pow(x1 - x2,2) - возведение во 2-ю степень
+
+Console.WriteLine($"Расстояние между точками A и B равно: {Math.Round(dist)}"); // Math.Round() - округляет значение аргумента до целых
