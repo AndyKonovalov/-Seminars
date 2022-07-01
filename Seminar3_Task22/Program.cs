bfd2ff08d5ef8dac7a1d68b5e4e78eb679dfff32
Andrei <fmwar92@gmail.com>
2022-07-01 13:43:14 +0300

    Worked only with 'for'

--- Seminar3_Task22/Program.cs
+++ Seminar3_Task22/Program.cs
@@ -1,12 +1,16 @@
 ﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу квадратов чисел от 1 до N.
 Console.Write("Введите число N: ");
 int N = Convert.ToInt32(Console.ReadLine());
-int i = 1; // числа от 1 до N
-int d = 0; // квадрат числа N
+//int i = 1; // числа от 1 до N
+//int d = 0; // квадрат числа N
 
-while (i <= N) // for(int i = 1; i <= N; i++) - цикл for позволяет компактнее записать цикл while
+// while (i <= N) 
+// {
+//     d = i * i;
+//     Console.WriteLine($"Квадрат числа {i} = {d}");
+//     i++;
+// }
+for(int i = 1; i <= N; i++) // for(int i = 1; i <= N; i++) - цикл for позволяет компактнее записать цикл while
 {
-    d = i * i;
-    Console.WriteLine($"Квадрат числа {i} = {d}");
-    i++;
+    Console.WriteLine($"Квадрат числа {i} = {i*i}");
 }
